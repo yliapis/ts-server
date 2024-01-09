5098ddd23f2ca5883e4e147597b0c7cac055390e
yliapis <yliapis44@gmail.com>
2024-01-08 21:16:32 -0600

    add create and read routes to app

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,92 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+
+// app setup
+
 const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
+
+// internal storage; volatile db for now
+
+const widgets: Widget[] = [];
+
+// Create
+
+app.post('/widget', (req: Request, res: Response) => {
+  res.send('POST HTTP method on widget resource');
+});
+
+interface Widget {
+  id: number;
+  name: string;
+  createdAt: Date;
+}
+
+interface CreateWidgetRequest {
+  name: string;
+}
+
+interface CreateWidgetResponse {
+  widget?: Widget;
+  status: string;
+}
+
+app.post('/widget', (req: Request<{}, {}, CreateWidgetRequest>, res: Response<CreateWidgetResponse>) => {
+  // Access the request body
+  const { name } = req.body;
+
+  const randomNumber: number = Math.floor(Math.random() * 1000000000000000)
+
+  const widget: Widget = {
+    id: randomNumber,
+    name: name,
+    createdAt: new Date()
+  }
+
+  // Add the widget to the list
+  widgets.push(widget);
+
+  // Create a response object
+  const createWidgetResponse: CreateWidgetResponse = {
+    widget: widget,
+    status: 'success'
+  };
+
+  // Send the response as JSON
+  res.json(createWidgetResponse);
+});
+// Generate a random number with 16 digits
+;
+
+
+// Read
+
+interface ListWidgetsResponse {
+  widgets: Widget[];
+  status: string;
+}
+
+app.get('/widget', (req: Request, res: Response<ListWidgetsResponse>) => {
+  // Retrieve all widgets
+  const allWidgets: Widget[] = widgets;
+
+  // Create a response object
+  const listWidgetsResponse: ListWidgetsResponse = {
+    widgets: allWidgets,
+    status: 'success'
+  };
+
+  // Send the response as JSON
+  res.json(listWidgetsResponse);
+});
+
+
+// begin listening
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
